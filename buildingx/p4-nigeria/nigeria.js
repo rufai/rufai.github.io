991ecea00229a49e61738ef8706c4648e4c345ff
howtobuildx <htbuildx@gmail.com>
2020-10-27 23:25:34 +0100

    added boostrap to improve presentation

--- buildingx/p4-nigeria/nigeria.js
+++ buildingx/p4-nigeria/nigeria.js
@@ -5,7 +5,10 @@ let nigeria_state = document.getElementById('states')
 let state_local_government = document.getElementById('lga')
 let state_local_government_list = document.getElementById('lga-list')
 
-for( index in nigeria )
+let state_name = document.getElementById('state_name')
+let number_of_lgas = document.getElementById('number_of_lgas')
+
+for( let index in nigeria )
 {
 	//console.log(state, nigeria[state].state.name)
 
@@ -18,15 +21,21 @@ for( index in nigeria )
 }
 
 nigeria_state.addEventListener('click', (e) => {
-	console.log(e, nigeria_state.value )
+	console.log(e, nigeria_state )
 
 	if(nigeria_state.value)
 	{
 		let state_index = parseInt( nigeria_state.value ) - 1
 
-		let state_lga = nigeria[state_index].state.locals
 
-		console.log(state_index, state_lga, state_lga[0].name)
+		// console.log(e, nigeria[ state_index ].state.name )
+
+		state_name.textContent = nigeria[ state_index ].state.name
+
+
+		let state_lga = nigeria[state_index].state.locals
+		number_of_lgas.textContent = state_lga.length
+		// console.log(state_index, state_lga, state_lga[0].name)
 
 		state_local_government_list.innerHTML = ""
 		state_local_government.innerHTML = ""
